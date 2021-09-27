846a54bd0e71a364dc5582b5c7d63b818ff7fb43
IreneBioque <irenebioque@gmail.com>
2021-09-27 17:14:06 +0200

    add selects d100 and d20

--- src/js/main.js
+++ src/js/main.js
@@ -7,12 +7,15 @@ const buttonD10 = document.querySelector('.js_buttond10');
 const buttonD8 = document.querySelector('.js_buttond8');
 const buttonD6 = document.querySelector('.js_buttond6');
 const buttonD4 = document.querySelector('.js_buttond4');
+const selectd100 = document.querySelector('.js_selectd100');
+const selectd20 = document.querySelector('.js_selectd20');
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-// const randomNumberD100 = getRandomNumber(100);
+// const randomNumberD100 = getRandomNumber(100) * 3;
+//  console.log(randomNumberD100);
 // const randomNumberD20 = getRandomNumber(20);
 // const randomNumberD12 = getRandomNumber(12);
 // const randomNumberD10 = getRandomNumber(10);
@@ -20,15 +23,20 @@ function getRandomNumber(max) {
 // const randomNumberD6 = getRandomNumber(6);
 // const randomNumberD4 = getRandomNumber(4);
 
+const selectValued100 = [];
+const selectValued20 = [];
+
+console.log(selectValued100);
 
 function handleRoll(ev) {
   const id = ev.currentTarget.id;
+
   if (id === 'd100') {
     const paragraphd100 = document.querySelector('.js_paragraphd100');
-    paragraphd100.innerHTML = getRandomNumber(100);
+    paragraphd100.innerHTML = getRandomNumber(100) * selectValued100;
   } else if (id === 'd20') {
     const paragraphd20 = document.querySelector('.js_paragraphd20');
-    paragraphd20.innerHTML = getRandomNumber(20);
+    paragraphd20.innerHTML = getRandomNumber(20) * selectValued20;
   } else if (id === 'd12') {
     const paragraphd12 = document.querySelector('.js_paragraphd12');
     paragraphd12.innerHTML = getRandomNumber(12);
@@ -48,6 +56,27 @@ function handleRoll(ev) {
 
 }
 
+function handleSelect(ev) {
+  const select = parseInt(ev.currentTarget.value);
+  const id = ev.currentTarget.id;
+  if (id === 'dicesd100') {
+    if (selectValued100.length === 0) {
+      selectValued100.push(select);
+    } else if (selectValued100.length !== 0) {
+      selectValued100.splice(0);
+      selectValued100.push(select);
+    }
+  } else if (id === 'dicesd20') {
+    if (selectValued20.length === 0) {
+      selectValued20.push(select);
+    } else if (selectValued20.length !== 0) {
+      selectValued20.splice(0);
+      selectValued20.push(select);
+    }
+  }
+  console.log(selectValued100);
+  console.log(selectValued20);
+}
 
 
 buttonD100.addEventListener('click', handleRoll);
@@ -57,3 +86,7 @@ buttonD10.addEventListener('click', handleRoll);
 buttonD8.addEventListener('click', handleRoll);
 buttonD6.addEventListener('click', handleRoll);
 buttonD4.addEventListener('click', handleRoll);
+
+
+selectd100.addEventListener('change', handleSelect);
+selectd20.addEventListener('change', handleSelect);
