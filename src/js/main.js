bd877a9a63250741bc8ad88a9dffd9f62e9aeee2
IreneBioque <irenebioque@gmail.com>
2021-09-27 21:32:43 +0200

    change style all

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,9 @@
 'use strict';
 
-const buttonD100 = document.querySelector('.js_buttond100');
-const buttonD20 = document.querySelector('.js_buttond20');
-const buttonD12 = document.querySelector('.js_buttond12');
-const buttonD10 = document.querySelector('.js_buttond10');
-const buttonD8 = document.querySelector('.js_buttond8');
-const buttonD6 = document.querySelector('.js_buttond6');
-const buttonD4 = document.querySelector('.js_buttond4');
+const button = document.querySelector('.js_button');
 const selectd100 = document.querySelector('.js_selectd100');
 const selectd20 = document.querySelector('.js_selectd20');
+const liElement = document.querySelectorAll('.js_liDice');
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
@@ -28,33 +23,37 @@ const selectValued20 = [];
 
 console.log(selectValued100);
 
-function handleRoll(ev) {
-  const id = ev.currentTarget.id;
-
-  if (id === 'd100') {
-    const paragraphd100 = document.querySelector('.js_paragraphd100');
-    paragraphd100.innerHTML = getRandomNumber(100) * selectValued100;
-  } else if (id === 'd20') {
-    const paragraphd20 = document.querySelector('.js_paragraphd20');
-    paragraphd20.innerHTML = selectValued20 * (getRandomNumber(20));
-  } else if (id === 'd12') {
-    const paragraphd12 = document.querySelector('.js_paragraphd12');
-    paragraphd12.innerHTML = getRandomNumber(12);
-  } else if (id === 'd10') {
-    const paragraphd10 = document.querySelector('.js_paragraphd10');
-    paragraphd10.innerHTML = getRandomNumber(10);
-  } else if (id === 'd8') {
-    const paragraphd8 = document.querySelector('.js_paragraphd8');
-    paragraphd8.innerHTML = getRandomNumber(8);
-  } else if (id === 'd6') {
-    const paragraphd6 = document.querySelector('.js_paragraphd6');
-    paragraphd6.innerHTML = getRandomNumber(6);
-  } else {
-    const paragraphd4 = document.querySelector('.js_paragraphd4');
-    paragraphd4.innerHTML = getRandomNumber(4);
+function handleRoll() {
+  debugger;
+  for (let element of liElement) {
+    const id = element.id;
+    console.log(id);
+    if (id === 'd100') {
+      const paragraphd100 = document.querySelector('.js_paragraphd100');
+      paragraphd100.innerHTML = getRandomNumber(100) * selectValued100;
+    } else if (id === 'd20') {
+      const paragraphd20 = document.querySelector('.js_paragraphd20');
+      paragraphd20.innerHTML = selectValued20 * getRandomNumber(20);
+    } else if (id === 'd12') {
+      const paragraphd12 = document.querySelector('.js_paragraphd12');
+      paragraphd12.innerHTML = getRandomNumber(12);
+    } else if (id === 'd10') {
+      const paragraphd10 = document.querySelector('.js_paragraphd10');
+      paragraphd10.innerHTML = getRandomNumber(10);
+    } else if (id === 'd8') {
+      const paragraphd8 = document.querySelector('.js_paragraphd8');
+      paragraphd8.innerHTML = getRandomNumber(8);
+    } else if (id === 'd6') {
+      const paragraphd6 = document.querySelector('.js_paragraphd6');
+      paragraphd6.innerHTML = getRandomNumber(6);
+    } else {
+      const paragraphd4 = document.querySelector('.js_paragraphd4');
+      paragraphd4.innerHTML = getRandomNumber(4);
+    }
   }
-
 }
+
+
 
 function handleSelect(ev) {
   const select = parseInt(ev.currentTarget.value);
@@ -79,14 +78,6 @@ function handleSelect(ev) {
 }
 
 
-buttonD100.addEventListener('click', handleRoll);
-buttonD20.addEventListener('click', handleRoll);
-buttonD12.addEventListener('click', handleRoll);
-buttonD10.addEventListener('click', handleRoll);
-buttonD8.addEventListener('click', handleRoll);
-buttonD6.addEventListener('click', handleRoll);
-buttonD4.addEventListener('click', handleRoll);
-
-
+button.addEventListener('click', handleRoll);
 selectd100.addEventListener('change', handleSelect);
 selectd20.addEventListener('change', handleSelect);
