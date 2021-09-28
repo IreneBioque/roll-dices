c8c3dbbf3a79ff88d1576cf8fc5768984a422993
IreneBioque <irenebioque@gmail.com>
2021-09-28 21:42:02 +0200

    add total result dices

--- src/js/main.js
+++ src/js/main.js
@@ -17,14 +17,16 @@ function handleRoll(number, faces) {
 
 
 function handleRolld100() {
+
   let valueInput = document.querySelector('#inputd100').value;
   if (valueInput !== '') {
+    const resultsAll = handleRoll(valueInput, 100);
     let resultsd100 = document.createElement('p');
-    resultsd100.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(
-      valueInput,
-      100
-    )}`;
-
+    let suma = 0;
+    for (const result of resultsAll) {
+      suma += parseInt(result);
+      resultsd100.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${resultsAll} El total es ${suma} `;
+    }
     showResults.appendChild(resultsd100);
   }
 }
@@ -32,22 +34,26 @@ function handleRolld100() {
 function handleRolld20() {
   let valueInput = document.querySelector('#inputd20').value;
   if (valueInput !== '') {
+    const resultsAll = handleRoll(valueInput, 20);
     let resultsd20 = document.createElement('p');
-    resultsd20.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(
-      valueInput,
-      20
-    )}`;
+    let suma = 0;
+    for (const result of resultsAll) {
+      suma += parseInt(result);
+      resultsd20.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${resultsAll} El total es ${suma} `;
+    }
     showResults.appendChild(resultsd20);
   }
 }
 function handleRolld12() {
   let valueInput = document.querySelector('#inputd12').value;
   if (valueInput !== '') {
+    const resultsAll = handleRoll(valueInput, 12);
     let resultsd12 = document.createElement('p');
-    resultsd12.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(
-      valueInput,
-      12
-    )}`;
+    let suma = 0;
+    for (const result of resultsAll) {
+      suma += parseInt(result);
+      resultsd12.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${resultsAll} El total es ${suma} `;
+    }
     showResults.appendChild(resultsd12);
   }
 }
@@ -55,46 +61,54 @@ function handleRolld12() {
 function handleRolld10() {
   let valueInput = document.querySelector('#inputd10').value;
   if (valueInput !== '') {
+    const resultsAll = handleRoll(valueInput, 10);
     let resultsd10 = document.createElement('p');
-    resultsd10.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(
-      valueInput,
-      10
-    )}`;
+    let suma = 0;
+    for (const result of resultsAll) {
+      suma += parseInt(result);
+      resultsd10.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${resultsAll} El total es ${suma} `;
+    }
     showResults.appendChild(resultsd10);
   }
 }
 function handleRolld8() {
   let valueInput = document.querySelector('#inputd8').value;
   if (valueInput !== '') {
-    let inputd8 = document.createElement('p');
-    inputd8.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(
-      valueInput,
-      8
-    )}`;
-    showResults.appendChild(inputd8);
+    const resultsAll = handleRoll(valueInput, 8);
+    let resultsd8 = document.createElement('p');
+    let suma = 0;
+    for (const result of resultsAll) {
+      suma += parseInt(result);
+      resultsd8.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${resultsAll} El total es ${suma} `;
+    }
+    showResults.appendChild(resultsd8);
   }
 }
 
 function handleRolld6 (){
   let valueInput = document.querySelector('#inputd6').value;
   if (valueInput !== '') {
-    let inputd6 = document.createElement('p');
-    inputd6.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(
-      valueInput,
-      6
-    )}`;
-    showResults.appendChild(inputd6);
+    const resultsAll = handleRoll(valueInput, 6);
+    let resultsd6 = document.createElement('p');
+    let suma = 0;
+    for (let i = 0; i < resultsAll.length; i++) {
+      suma += parseInt(resultsAll[i]);
+      resultsd6.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${resultsAll} El total es ${suma} `;
+    }
+    showResults.appendChild(resultsd6);
   }
 }
 function handleRolld4() {
   let valueInput = document.querySelector('#inputd4').value;
   if (valueInput !== '') {
-    let inputd4 = document.createElement('p');
-    inputd4.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(
-      valueInput,
-      4
-    )}`;
-    showResults.appendChild(inputd4);
+    const resultsAll = handleRoll(valueInput, 4);
+    let resultsd4 = document.createElement('p');
+    let suma = 0;
+    for (let i = 0; i < resultsAll.length; i++) {
+      suma += parseInt(resultsAll[i]);
+      resultsd4.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${resultsAll} El total es ${suma} `;
+    }
+    showResults.appendChild(resultsd4);
   }
 }
 function renderRoll() {
