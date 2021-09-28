4b8128333013dff9c166abb434b7501d704ea3d7
IreneBioque <irenebioque@gmail.com>
2021-09-28 19:54:30 +0200

    optimize functions js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 'use strict';
-
+let showResults = document.querySelector('.js_results');
 const button = document.querySelector('.js_button');
 
 
@@ -17,51 +17,96 @@ function handleRoll(number, faces) {
 }
 
 
-function handleInput() {
-  let showResults = document.querySelector('.js_results');
-  showResults.innerHTML = '';
+function handleRolld100() {
   let valueInput = document.querySelector('#inputd100').value;
   if (valueInput !== '') {
     let resultsd100 = document.createElement('p');
-    resultsd100.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(valueInput, 100)}`;
+    resultsd100.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(
+      valueInput,
+      100
+    )}`;
 
     showResults.appendChild(resultsd100);
   }
-  valueInput = document.querySelector('#inputd20').value;
+}
+
+function handleRolld20() {
+  let valueInput = document.querySelector('#inputd20').value;
   if (valueInput !== '') {
     let resultsd20 = document.createElement('p');
-    resultsd20.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(valueInput,20)}`;
+    resultsd20.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(
+      valueInput,
+      20
+    )}`;
     showResults.appendChild(resultsd20);
   }
-  valueInput = document.querySelector('#inputd12').value;
+}
+function handleRolld12() {
+  let valueInput = document.querySelector('#inputd12').value;
   if (valueInput !== '') {
     let resultsd12 = document.createElement('p');
-    resultsd12.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(valueInput,12)}`;
+    resultsd12.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(
+      valueInput,
+      12
+    )}`;
     showResults.appendChild(resultsd12);
   }
-  valueInput = document.querySelector('#inputd10').value;
+}
+
+function handleRolld10() {
+  let valueInput = document.querySelector('#inputd10').value;
   if (valueInput !== '') {
     let resultsd10 = document.createElement('p');
-    resultsd10.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(valueInput,12)}`;
+    resultsd10.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(
+      valueInput,
+      10
+    )}`;
     showResults.appendChild(resultsd10);
   }
-  valueInput = document.querySelector('#inputd8').value;
+}
+function handleRolld8() {
+  let valueInput = document.querySelector('#inputd8').value;
   if (valueInput !== '') {
     let inputd8 = document.createElement('p');
-    inputd8.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(valueInput,8)}`;
+    inputd8.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(
+      valueInput,
+      8
+    )}`;
     showResults.appendChild(inputd8);
   }
-  valueInput = document.querySelector('#inputd6').value;
+}
+
+function handleRolld6 (){
+  let valueInput = document.querySelector('#inputd6').value;
   if (valueInput !== '') {
     let inputd6 = document.createElement('p');
-    inputd6.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(valueInput,6)}`;
+    inputd6.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(
+      valueInput,
+      6
+    )}`;
     showResults.appendChild(inputd6);
   }
-  valueInput = document.querySelector('#inputd4').value;
+}
+function handleRolld4() {
+  let valueInput = document.querySelector('#inputd4').value;
   if (valueInput !== '') {
     let inputd4 = document.createElement('p');
-    inputd4.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(valueInput,4)}`;
+    inputd4.innerHTML = `Tiraste ${valueInput} dados, los resultados son ${handleRoll(
+      valueInput,
+      4
+    )}`;
     showResults.appendChild(inputd4);
   }
 }
-button.addEventListener('click', handleInput);
+function renderRoll() {
+  showResults.innerHTML = '';
+  handleRolld100();
+  handleRolld20();
+  handleRolld12();
+  handleRolld10();
+  handleRolld8();
+  handleRolld6();
+  handleRolld4();
+}
+
+button.addEventListener('click', renderRoll);
