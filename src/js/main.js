2cdcc1ce06b6fa8e0ac4e86b81cb5a9535c31ae7
IreneBioque <irenebioque@gmail.com>
2021-09-29 20:36:12 +0200

    add plus and rest buttons

--- src/js/main.js
+++ src/js/main.js
@@ -2,26 +2,16 @@
 const showResults = document.querySelector('.js_results');
 const button = document.querySelector('.js_button');
 const save = document.querySelector('.js_save');
-// const liElement = document.querySelectorAll('.js_liDice');
 const buttonSave = document.querySelector('.js_buttonSave');
+const valueInputd100 = document.querySelector('#inputd100');
+const valueInputd20 = document.querySelector('#inputd20');
+const valueInputd12 = document.querySelector('#inputd12');
+const valueInputd10 = document.querySelector('#inputd10');
+const valueInputd8 = document.querySelector('#inputd8');
+const valueInputd6 = document.querySelector('#inputd6');
+const valueInputd4 = document.querySelector('#inputd4');
 
-// for (const element of liElement) {
-//   element.addEventListener('click', handleSelect);
-// }
 
-// function handleSelect(ev) {
-//   save.innerHTML = '';
-//   const id = ev.currentTarget.id;
-//   console.log(ev.currentTarget.id);
-//   if (id === '100') {
-//     const html = ` <h3>Tirada guardada</h3> <li > <label for="">
-//             <i class="fas fa-dice-d20 js_liDice" id=${id} ></i>
-//             <input class="js_inputd100" type="number" name="" id="inputd100" value=2 />
-//           </label>
-//         </li>`;
-//     save.innerHTML += html;
-//   }
-// }
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
@@ -35,96 +25,91 @@ function handleRoll(number, faces) {
   return diceArray;
 }
 
+
 function handleRolld100() {
-  let valueInput = document.querySelector('#inputd100').value;
-  if (valueInput !== '') {
-    const resultsAll = handleRoll(valueInput, 100);
+
+  if (valueInputd100.value !== '') {
+    const resultsAll = handleRoll(valueInputd100.value, 100);
     let resultsd100 = document.createElement('p');
     let suma = 0;
     for (const result of resultsAll) {
       suma += parseInt(result);
-      resultsd100.innerHTML = `Tiraste ${valueInput} dados d100, los resultados son ${resultsAll} El total es ${suma} `;
+      resultsd100.innerHTML = `Tiraste ${valueInputd100.value} dados d100, los resultados son ${resultsAll} El total es ${suma} `;
     }
     showResults.appendChild(resultsd100);
   }
 }
 
 function handleRolld20() {
-  let valueInput = document.querySelector('#inputd20').value;
-  if (valueInput !== '') {
-    const resultsAll = handleRoll(valueInput, 20);
+  if (valueInputd20.value !== '') {
+    const resultsAll = handleRoll(valueInputd20.value, 20);
     let resultsd20 = document.createElement('p');
     let suma = 0;
     for (const result of resultsAll) {
       suma += parseInt(result);
-      resultsd20.innerHTML = `Tiraste ${valueInput} dados d20, los resultados son ${resultsAll} El total es ${suma} `;
+      resultsd20.innerHTML = `Tiraste ${valueInputd20.value} dados d20, los resultados son ${resultsAll} El total es ${suma} `;
     }
     showResults.appendChild(resultsd20);
   }
 }
 function handleRolld12() {
-  let valueInput = document.querySelector('#inputd12').value;
-  if (valueInput !== '') {
-    const resultsAll = handleRoll(valueInput, 12);
+  if (valueInputd12.value !== '') {
+    const resultsAll = handleRoll(valueInputd12.value, 12);
     let resultsd12 = document.createElement('p');
     let suma = 0;
     for (const result of resultsAll) {
       suma += parseInt(result);
-      resultsd12.innerHTML = `Tiraste ${valueInput} dados d12, los resultados son ${resultsAll} El total es ${suma} `;
+      resultsd12.innerHTML = `Tiraste ${valueInputd12.value} dados d12, los resultados son ${resultsAll} El total es ${suma} `;
     }
     showResults.appendChild(resultsd12);
   }
 }
 
 function handleRolld10() {
-  let valueInput = document.querySelector('#inputd10').value;
-  if (valueInput !== '') {
-    const resultsAll = handleRoll(valueInput, 10);
+  if (valueInputd10.value !== '') {
+    const resultsAll = handleRoll(valueInputd10.value, 10);
     let resultsd10 = document.createElement('p');
     let suma = 0;
     for (const result of resultsAll) {
       suma += parseInt(result);
-      resultsd10.innerHTML = `Tiraste ${valueInput} dados d10, los resultados son ${resultsAll} El total es ${suma} `;
+      resultsd10.innerHTML = `Tiraste ${valueInputd10.value} dados d10, los resultados son ${resultsAll} El total es ${suma} `;
     }
     showResults.appendChild(resultsd10);
   }
 }
 function handleRolld8() {
-  let valueInput = document.querySelector('#inputd8').value;
-  if (valueInput !== '') {
-    const resultsAll = handleRoll(valueInput, 8);
+  if (valueInputd8.value !== '') {
+    const resultsAll = handleRoll(valueInputd8.value, 8);
     let resultsd8 = document.createElement('p');
     let suma = 0;
     for (const result of resultsAll) {
       suma += parseInt(result);
-      resultsd8.innerHTML = `Tiraste ${valueInput} dados d8, los resultados son ${resultsAll} El total es ${suma} `;
+      resultsd8.innerHTML = `Tiraste ${valueInputd8.value} dados d8, los resultados son ${resultsAll} El total es ${suma} `;
     }
     showResults.appendChild(resultsd8);
   }
 }
 
 function handleRolld6() {
-  let valueInput = document.querySelector('#inputd6').value;
-  if (valueInput !== '') {
-    const resultsAll = handleRoll(valueInput, 6);
+  if (valueInputd6.value !== '') {
+    const resultsAll = handleRoll(valueInputd6.value, 6);
     let resultsd6 = document.createElement('p');
     let suma = 0;
     for (let i = 0; i < resultsAll.length; i++) {
       suma += parseInt(resultsAll[i]);
-      resultsd6.innerHTML = `Tiraste ${valueInput} dados d6, los resultados son ${resultsAll} El total es ${suma} `;
+      resultsd6.innerHTML = `Tiraste ${valueInputd6.value} dados d6, los resultados son ${resultsAll} El total es ${suma} `;
     }
     showResults.appendChild(resultsd6);
   }
 }
 function handleRolld4() {
-  let valueInput = document.querySelector('#inputd4').value;
-  if (valueInput !== '') {
-    const resultsAll = handleRoll(valueInput, 4);
+  if (valueInputd4.value !== '') {
+    const resultsAll = handleRoll(valueInputd4.value, 4);
     let resultsd4 = document.createElement('p');
     let suma = 0;
     for (let i = 0; i < resultsAll.length; i++) {
       suma += parseInt(resultsAll[i]);
-      resultsd4.innerHTML = `Tiraste ${valueInput} dados d4, los resultados son ${resultsAll} El total es ${suma} `;
+      resultsd4.innerHTML = `Tiraste ${valueInputd4.value} dados d4, los resultados son ${resultsAll} El total es ${suma} `;
     }
     showResults.appendChild(resultsd4);
   }
@@ -174,3 +159,79 @@ function handleSelectSave() {
   save.classList.toggle('hidden');
 }
 buttonSave.addEventListener('click', handleSelectSave);
+
+const buttonPlus = document.querySelectorAll('.js_buttonplus');
+const buttonRest = document.querySelectorAll('.js_buttonrest');
+
+for (const buttonP of buttonPlus) {
+  buttonP.addEventListener('click', incrementar);
+}
+for (const buttonR of buttonRest) {
+  buttonR.addEventListener('click', decrement);
+}
+
+
+let counterd100 = 0;
+let counterd20 = 0;
+let counterd12 = 0;
+let counterd10 = 0;
+let counterd8 = 0;
+let counterd6 = 0;
+let counterd4 = 0;
+
+
+
+function incrementar(ev) {
+  ev.preventDefault();
+  const id = ev.currentTarget.id;
+  if (id === 'buttonplusd100') {
+    counterd100 = counterd100 + 1;
+    valueInputd100.value = counterd100;
+  } else if (id === 'buttonplusd20') {
+    counterd20 = counterd20 + 1;
+    valueInputd20.value = counterd20;
+  } else if (id === 'buttonplusd12') {
+    counterd12 = counterd12 + 1;
+    valueInputd12.value = counterd12;
+  } else if (id === 'buttonplusd10') {
+    counterd10 = counterd10 + 1;
+    valueInputd10.value = counterd10;
+  } else if (id === 'buttonplusd8') {
+    counterd8 = counterd8 + 1;
+    valueInputd8.value = counterd8;
+  } else if (id === 'buttonplusd6') {
+    counterd6 = counterd6 + 1;
+    valueInputd6.value = counterd6;
+  } else {
+    counterd4 = counterd4 + 1;
+    valueInputd4.value = counterd4;
+  }
+}
+function decrement(ev) {
+  ev.preventDefault();
+  const id = ev.currentTarget.id;
+  if (id === 'buttonrestd100') {
+    counterd100 = counterd100 - 1;
+    valueInputd100.value = counterd100;
+  } else if (id === 'buttonrestd20') {
+    counterd20 = counterd20 - 1;
+    valueInputd20.value = counterd20;
+  } else if (id === 'buttonrestd12') {
+    counterd12 = counterd12 - 1;
+    valueInputd12.value = counterd12;
+  } else if (id === 'buttonrestd10') {
+    counterd10 = counterd10 - 1;
+    valueInputd10.value = counterd10;
+  } else if (id === 'buttonrestd8') {
+    counterd8 = counterd8 - 1;
+    valueInputd8.value = counterd8;
+  } else if (id === 'buttonrestd6') {
+    counterd6 = counterd6 - 1;
+    valueInputd6.value = counterd6;
+  } else {
+    counterd4 = counterd4 + 1;
+    valueInputd4.value = counterd4;
+  }
+
+}
+
